d0eb698f2548a5678edfcc341cbeecfcc6c5c5ee
Tristan Crockett <tristan.h.crockett@gmail.com>
2020-10-02 23:05:08 -0500

    Residential area (without people)

--- core/assets/maps/farming_fishing.tsx
+++ core/assets/maps/farming_fishing.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="farming_fishing" tilewidth="16" tileheight="32" tilecount="100" columns="10">
- <image source="tiles/farming_fishing.png" width="320" height="320"/>
+<tileset version="1.4" tiledversion="1.4.2" name="farming_fishing" tilewidth="16" tileheight="24" tilecount="260" columns="20">
+ <image source="../../../../../../../home/thcrock/src/SuikodenRM/core/assets/maps/tiles/farming_fishing.png" width="320" height="320"/>
 </tileset>
