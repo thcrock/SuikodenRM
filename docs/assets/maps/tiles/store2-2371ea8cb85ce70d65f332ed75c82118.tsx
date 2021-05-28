8e5fd15f9b24ac550827cda1520dbb339a929d01
Tristan Crockett <tristan.h.crockett@gmail.com>
2021-05-27 22:54:39 -0500

    Clear docs

--- docs/assets/maps/tiles/store2-2371ea8cb85ce70d65f332ed75c82118.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="store2" tilewidth="80" tileheight="68" tilecount="1" columns="1">
- <image source="store2.png" width="80" height="68"/>
-</tileset>
--- docs/assets/maps/tileset_town_multi_v002-5aab11094e2a2cffa8385ba8827d4404.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="tileset_town_multi_v002" tilewidth="16" tileheight="16" tilecount="2240" columns="16">
- <image source="tiles/tileset_town_multi_v002.png" width="256" height="2240"/>
-</tileset>
